3b1bb1fb2b10d73ab28433837c8241e67407cfae
ryanpham48 <anhvu.network@gmail.com>
2024-12-12 05:00:27 +0800

    Update Project2_app.py

--- Project2_app.py
+++ Project2_app.py
@@ -41,10 +41,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 # GUI
-st.title("Data Science Project")
-st.write("## Hệ thống gợi ý sản phẩm mỹ phẩm")
+st.title("DATA SCIENCE PROJECT")
+st.write("## Hệ Thống Gợi Ý Sản Phẩm")
 
-menu = ["Phát biểu bài toán", "Phương pháp giải quyết bài toán", "Gợi ý sản phẩm theo thông tin khách hàng", "Gợi ý sản phẩm theo thông tin sản phẩm"]
+menu = ["Tổng Quan", "Thực Hiện & Đánh Giá Model", "Gợi ý theo thông tin khách hàng", "Gợi ý sản theo thông tin sản phẩm"]
 choice = st.sidebar.selectbox('Menu', menu)
 st.sidebar.write("""#### Thành viên thực hiện:
                  Lý Quốc Hồng Phúc & Phạm Anh Vũ""")
@@ -54,46 +54,28 @@ st.sidebar.write("""#### Giảng viên hướng dẫn: Cô Khuất Thùy Phươn
 st.sidebar.image('khuat_thuy_phuong.jpg')
 st.sidebar.write("""#### Thời gian thực hiện: 12/2024""")
 
-if choice == 'Phát biểu bài toán':
+if choice == 'Tổng Quan':
     # Giao diện Streamlit
-    st.title("Phát biểu bài toán")
+    st.title("Yêu Cầu Thực Hiện")
     st.image('hasaki_banner.jpg', use_container_width=True)
     # Nội dung phát biểu bài toán
     st.write("""
-    **Công ty Hasaki mong muốn xây dựng một hệ thống đề xuất sản phẩm mỹ phẩm nhằm cá nhân hóa trải nghiệm người dùng, giúp khách hàng dễ dàng tìm kiếm và lựa chọn sản phẩm phù hợp với sở thích và nhu cầu của họ. Hệ thống này sẽ phân tích dữ liệu về sản phẩm và hành vi của người dùng để đưa ra các gợi ý hiệu quả, tăng cường sự hài lòng của khách hàng và thúc đẩy doanh số bán hàng.**
+    **Công ty Hasaki mong muốn xây dựng một hệ thống đề xuất sản phẩm nhằm cá nhân hóa trải nghiệm người dùng, giúp khách hàng dễ dàng tìm kiếm và lựa chọn sản phẩm phù hợp với sở thích và nhu cầu của họ. Hệ thống này sẽ phân tích dữ liệu về sản phẩm và hành vi của người dùng để đưa ra các gợi ý hiệu quả, tăng cường sự hài lòng của khách hàng và thúc đẩy doanh số bán hàng.**
 
     **Cụ thể, bài toán đặt ra là:**
-    1. Với khách hàng mới (chưa có nhiều tương tác với hệ thống), hệ thống cần sử dụng thông tin về sản phẩm để đề xuất các sản phẩm tương tự.
-    2. Với khách hàng đã có lịch sử mua sắm hoặc tương tác: hệ thống cần dựa trên thông tin mua sắm và nội dung đánh giá của những người dùng khác có sở thích tương tự để đưa ra gợi ý chính xác hơn.
+    1. Với khách hàng đã có lịch sử mua sắm hoặc tương tác: hệ thống cần dựa trên thông tin mua sắm và nội dung đánh giá của những người dùng khác có sở thích tương tự để đưa ra gợi ý chính xác hơn.
+    2. Với khách hàng mới (chưa có nhiều tương tác với hệ thống), hệ thống cần sử dụng thông tin về sản phẩm để đề xuất các sản phẩm tương tự.
     """)
 
-elif choice == 'Phương pháp giải quyết bài toán':
+elif choice == 'Thực Hiện & Đánh Giá Model':
     # Giao diện Streamlit
     st.title("Phương pháp giải quyết bài toán")
     st.image('Hasaki.logo.wide.jpg', use_container_width=True)
     # Nội dung phương pháp giải quyết bài toán
     st.markdown("""
-    **Để giải quyết bài toán trên, hệ thống sẽ sử dụng kết hợp hai phương pháp: **Content-based Filtering** và **Collaborative Filtering**.**
+    **Để giải quyết nhu cầu của Hasaki, hệ thống sẽ sử dụng kết hợp hai phương pháp: **Content-based Filtering** và **Collaborative Filtering**.** 
 
-    I. Content-based Filtering:  
-    > * Nguyên lý: Phân tích thông tin về sản phẩm (như thành phần, công dụng, loại da phù hợp, giá cả, v.v.) để tìm các sản phẩm tương tự dựa trên đặc trưng của chúng.  
-    > * Thuật toán: Sử dụng thuật toán Cosine Similarity để tính độ tương đồng giữa các sản phẩm dựa trên các vector đặc trưng bằng cách tính góc giữa hai vector đặc trưng để xác định độ tương đồng. Giá trị Cosine Similarity càng gần 1 thì độ tương đồng giữa hai sản phẩm càng cao.  
-    > * Các bước xây dựng mô hình cho thuật toán Cosine Similarity:  
-    >> 1. Dữ liệu đầu vào của mô hình là nội dung mô tả sản phẩm đã được tiền xử lý.  
-    >> 2. Vector hóa nội dung mô tả sản phẩm bằng TF-IDF.  
-    >> 3. Tính toán ma trận tương đồng cosine.  
-    >> 4. Dữ liệu đầu ra của mô hình là ma trận tương đồng cosine.  
-    >> 5. Lưu mô hình.  
-    >> 6. Viết hàm đề xuất sản phẩm (get_products_recommendations):  
-    >>> - Dữ liệu đầu vào của hàm đề xuất sản phẩm:  
-    >>>> + Dataframe của nội dung mô tả sản phẩm  
-    >>>> + Mã sản phẩm  
-    >>>> + Ma trận tương đồng cosine giữa các sản phẩm  
-    >>>> + Số lượng sản phẩm tương đồng cao nhất (chọn 3 sản phẩm)  
-    >>> - Dữ liệu đầu ra của hàm đề xuất sản phẩm:  
-    >>>> + Dataframe của 3 sản phẩm tương tự nhất với sản phẩm có mã sản phẩm được nhập.  
-
-    II. Collaborative Filtering:  
+    I. Collaborative Filtering:  
     > * Nguyên lý: Dựa vào hành vi của cộng đồng người dùng (lịch sử mua sắm và nội dung đánh giá), tìm kiếm các khách hàng có hành vi mua sắm hoặc đánh giá tương tự để tìm ra những mối liên hệ tiềm ẩn giữa khách hàng và sản phẩm mà Content-Based Filtering không thể, để đề xuất sản phẩm phù hợp cho người dùng.  
     > * Thuật toán: Sử dụng các mô hình từ thư viện Surprise để dự đoán điểm đánh giá sản phẩm cho người dùng. Các thuật toán được áp dụng bao gồm:  
     >> 1. SVD (Singular Value Decomposition)  
@@ -121,9 +103,26 @@ elif choice == 'Phương pháp giải quyết bài toán':
     >>>> + Mã khách hàng  
     >>>> + Số lượng sản phẩm đề xuất (chọn 5 sản phẩm)  
     >>> - Dữ liệu đầu ra của hàm đề xuất sản phẩm:  
-    >>>> + Dataframe của 5 sản phẩm được đề xuất có điểm đánh giá cao nhất ứng với mã khách hàng được nhập.""")
+    >>>> + Dataframe của 5 sản phẩm được đề xuất có điểm đánh giá cao nhất ứng với mã khách hàng được nhập.
+    II.  Content-based Filtering:  
+    > * Nguyên lý: Phân tích thông tin về sản phẩm (như thành phần, công dụng, loại da phù hợp, giá cả, v.v.) để tìm các sản phẩm tương tự dựa trên đặc trưng của chúng.  
+    > * Thuật toán: Sử dụng thuật toán Cosine Similarity để tính độ tương đồng giữa các sản phẩm dựa trên các vector đặc trưng bằng cách tính góc giữa hai vector đặc trưng để xác định độ tương đồng. Giá trị Cosine Similarity càng gần 1 thì độ tương đồng giữa hai sản phẩm càng cao.  
+    > * Các bước xây dựng mô hình cho thuật toán Cosine Similarity:  
+    >> 1. Dữ liệu đầu vào của mô hình là nội dung mô tả sản phẩm đã được tiền xử lý.  
+    >> 2. Vector hóa nội dung mô tả sản phẩm bằng TF-IDF.  
+    >> 3. Tính toán ma trận tương đồng cosine.  
+    >> 4. Dữ liệu đầu ra của mô hình là ma trận tương đồng cosine.  
+    >> 5. Lưu mô hình.  
+    >> 6. Viết hàm đề xuất sản phẩm (get_products_recommendations):  
+    >>> - Dữ liệu đầu vào của hàm đề xuất sản phẩm:  
+    >>>> + Dataframe của nội dung mô tả sản phẩm  
+    >>>> + Mã sản phẩm  
+    >>>> + Ma trận tương đồng cosine giữa các sản phẩm  
+    >>>> + Số lượng sản phẩm tương đồng cao nhất (chọn 4 sản phẩm)  
+    >>> - Dữ liệu đầu ra của hàm đề xuất sản phẩm:  
+    >>>> + Dataframe của 4 sản phẩm tương tự nhất với sản phẩm có mã sản phẩm được nhập. """)
 
-elif choice == 'Gợi ý sản phẩm theo thông tin khách hàng':    
+elif choice == 'Gợi ý theo thông tin khách hàng':    
     # Hàm để kiểm tra khách hàng và đề xuất sản phẩm
     def recommend_products_for_customer(ma_khach_hang, data_sub_pandas, products_sub_pandas, best_algorithm):
         # Kiểm tra nếu khách hàng đã đánh giá sản phẩm
@@ -223,7 +222,7 @@ elif choice == 'Gợi ý sản phẩm theo thông tin khách hàng':
             else:
                 st.error("Không tìm thấy thông tin khách hàng.")
     
-elif choice == 'Gợi ý sản phẩm theo thông tin sản phẩm':
+elif choice == 'Gợi ý theo thông tin sản phẩm':
     # Nhập tên sản phẩm, tìm kiếm mã sản phẩm, và đề xuất các sản phẩm liên quan
     def get_products_recommendations(products, product_id, cosine_sim, nums=5):
         # Tìm chỉ mục sản phẩm dựa trên mã sản phẩm
