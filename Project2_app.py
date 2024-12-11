68d1a84c98d7a0db79d17f51b4413656aa46dfc9
ryanpham48 <anhvu.network@gmail.com>
2024-12-12 05:30:41 +0800

    Update Project2_app.py

--- Project2_app.py
+++ Project2_app.py
@@ -47,8 +47,9 @@ menu = ["Tổng Quan", "Thực Hiện & Đánh Giá Model", "Gợi ý theo thôn
 choice = st.sidebar.selectbox('Menu', menu)
 st.sidebar.write("""#### Thành viên thực hiện:
                  Lý Quốc Hồng Phúc & Phạm Anh Vũ""")
+image_width = 400
 st.sidebar.image('phucly.png')
-st.sidebar.image('vupham.jpg')
+st.sidebar.image('vupham.jpg', width=image_width)
 st.sidebar.write("#### Giảng viên hướng dẫn: Cô Khuất Thùy Phương")
 st.sidebar.image('khuat_thuy_phuong.jpg')
 st.sidebar.write("""#### Thời gian thực hiện: 12/2024""")
